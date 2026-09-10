dff20220ea9ab26af647d44a1aa9123adef9b982
Mohit Nawani <mohitnawani10@gmail.com>
2026-09-10 12:52:59 +0530

    tailwinf.conifg changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,31 +35,32 @@ export default {
         proxima: ['Proxima Nova', 'Montserrat', 'sans-serif'],
         ddin: ['D-DIN', 'DIN Alternate', 'Montserrat', 'sans-serif'],
       },
-        fontSize: {
-          xs: ["12px", { lineHeight: "18px" }],
-          sm: ["14px", { lineHeight: "22px" }],
-          base: ["16px", { lineHeight: "26px" }],
-          lg: ["18px", { lineHeight: "30px" }],
-          xl: ["20px", { lineHeight: "30px" }],
-          "2xl": ["24px", { lineHeight: "34px" }],
-          "3xl": ["30px", { lineHeight: "38px" }],
-          "4xl": ["36px", { lineHeight: "42px" }],
-        },
+      fontSize: {
+        xs: ["12px", { lineHeight: "18px" }],
+        sm: ["14px", { lineHeight: "22px" }],
+        base: ["16px", { lineHeight: "26px" }],
+        lg: ["18px", { lineHeight: "30px" }],
+        xl: ["20px", { lineHeight: "30px" }],
+        "2xl": ["24px", { lineHeight: "34px" }],
+        "3xl": ["30px", { lineHeight: "38px" }],
+        "4xl": ["36px", { lineHeight: "42px" }],
+      },
+
       spacing: {
-        1: '0.375rem',   // 6px
-        2: '0.875rem',   // 14px
-        3: '1.375rem',   // 22px
-        4: '1.875rem',   // 30px
-        5: '2.375rem',    // 38px
-        6: '2.875rem',    // 46px
-        7: '3.375rem',    // 54px
-        8: '3.875rem',    // 62px
-        9: '4.375rem',    // 70px
-        10: '4.875rem',   // 78px
-        11: '5.375rem',   // 86px
-        12: '5.875rem',   // 94px
-        14: '6.875rem',   // 110px
-        16: '7.875rem',   // 126px
+        1: "0.375rem",   // 6px
+        2: "0.875rem",   // 14px
+        3: "1.375rem",   // 22px
+        4: "1.875rem",   // 30px
+        5: "2.375rem",   // 38px
+        6: "2.875rem",   // 46px
+        7: "3.375rem",   // 54px
+        8: "3.875rem",   // 62px
+        9: "4.375rem",   // 70px
+        10: "4.875rem",  // 78px
+        11: "5.375rem",  // 86px
+        12: "5.875rem",  // 94px
+        14: "6.875rem",  // 110px
+        16: "7.875rem",  // 126px
       },
     },
   },
